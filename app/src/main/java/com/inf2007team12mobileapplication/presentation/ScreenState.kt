5f130218d64832274b0b75662a067d882220ef1f
JasonTan <jasontky23@gmail.com>
2024-03-28 21:38:02 +0800

    home page

---
+++ app/src/main/java/com/inf2007team12mobileapplication/presentation/ScreenState.kt
@@ -0,0 +1,19 @@
+package com.inf2007team12mobileapplication.presentation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CameraAlt
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Report
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class Screen(val route: String, val icon: ImageVector? = null) {
+    object SignUp : Screen("signup")
+    object SignIn : Screen("signin")
+    object Profile : Screen("profile")
+    // Object CreateProfile : Screen("createprofile")
+    object Camera : Screen("camera", Icons.Default.CameraAlt)
+    object ResetPassword : Screen("resetpassword")
+    object Report : Screen("report", Icons.Default.Report)
+    object Home : Screen("home", Icons.Default.Home)
+
+}
